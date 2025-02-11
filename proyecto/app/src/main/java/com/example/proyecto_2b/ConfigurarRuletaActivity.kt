64e401fecc8c396638d002d1211b58e7eed68304
Leonardo <leonardo.quishpe@epn.edu.ec>
2025-02-10 19:18:30 -0500

    Deber 2 y Exam 2 BIM

--- proyecto/app/src/main/java/com/example/proyecto_2b/ConfigurarRuletaActivity.kt
+++ proyecto/app/src/main/java/com/example/proyecto_2b/ConfigurarRuletaActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
